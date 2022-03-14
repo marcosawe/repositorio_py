e5a0dbd90f3552f4f678feeb21c32f6c4b457505
Marcos Eduardo Fernandes Domingues <maresduardo420@gmail.com>
2022-03-14 12:56:03 -0300

    Split

--- fundamentos/split_join_enumerate/main.py
+++ fundamentos/split_join_enumerate/main.py
@@ -1,2 +1,16 @@
 """
+Split Join e Enumerate em Phyton
+* Split - Dividir uma string #str
+* Join - Juntar uam string #str
+* Enumerate - Enumerar elementos de uma lista #list / iteráveis 
 """
+strings = 'O Brasil é o pais do Futebol, o Brasil tem Penta.';
+lista_1 = strings.split(' ');
+lista_2 = strings.split(',')
+print(lista_1);
+print()
+print(lista_2);
+print()
+
+for valor in lista_1:
+    print(f'A palavra {valor} apareceu {lista_1.count(valor)}x na frase.');

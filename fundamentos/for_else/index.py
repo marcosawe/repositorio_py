10874c765c12bec0121262a8cfa7f4d2e206ac51
Marcos Eduardo Fernandes Domingues <maresduardo420@gmail.com>
2022-03-14 12:24:15 -0300

    For_else

---
+++ fundamentos/for_else/index.py
@@ -0,0 +1,9 @@
+"""
+For/Else - Phyton
+"""
+variavel = ['Marcão', 'Bixo Ruim', 'Humilde']
+for valor in variavel:
+    if valor.startswith('M'):
+        print('É o Marcão!!!', valor);
+    else:
+        print('Não é o Marcão ;(((', valor);

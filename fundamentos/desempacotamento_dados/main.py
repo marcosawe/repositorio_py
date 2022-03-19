d75fc9eb82ff8f0689c0ef448fabc0308ca70aa7
Marcos Eduardo Fernandes Domingues <maresduardo420@gmail.com>
2022-03-19 14:55:51 -0300

    dados

---
+++ fundamentos/desempacotamento_dados/main.py
@@ -0,0 +1,6 @@
+"""
+Desempacotamento de listas em Python
+"""
+lista = ['Luiz', 'João', 'Maria', 12,32,45,6];
+n1,n2,n3, *outralista = lista;
+print(n1,n3);
---
+++ fundamentos/trocando_valor_variavel/main.py
@@ -0,0 +1,5 @@
+# Basta implementar as variáveis na mesma linhas com vírgula e depois ordenar o valor da troca após o sinal de igual.
+x = 13
+y = 'Lula Vascaino'
+x,y = y,x
+print(f'x = {x} e y = {y}')

b1e36e65c30f2fb5949140862df4da9eb3df4754
Marcos Eduardo Fernandes Domingues <maresduardo420@gmail.com>
2022-05-19 13:54:51 -0300

    main

--- programacao_procedural/map/mapeamentos.py
+++ programacao_procedural/map/mapeamentos.py
@@ -1,13 +1,29 @@
-from dados import lista,produtos,pessoas
+# Mapa em Python é uma função que funciona como um iterador para retornar um resultado após aplicar uma função a cada item de um iterável (tupla, listas, etc.). É usado quando você deseja aplicar uma única função de transformação a todos os elementos iteráveis. A função iterável e a função são passadas como argumentos para o mapa em Python.
+
+"""
+A sintaxe da função Python map () é:
+
+map (função, iteráveis)
+
+Em a sintaxe acima:
+
+função: É a função de transformação através da qual todos os itens do iterável serão passados. iteráveis: É o iterável (sequência, coleção como lista ou tupla) que você deseja mapear.
+
+"""
+from dados import lista
+
 """TRABALHANDO COM LISTAS"""
+
 # Para fazer um mapeamento por operação utilizamos
 nova_lista = map(lambda x: x*2,lista);
 print(lista);
 print(list(nova_lista)); # A função map não retorna um lista pronta mas sim um iterador que pode sofrer casting.
 lista2 = [x * 2 for x in lista ] # Utilizando list comprehension
 print(lista2)
-"""TRABALHANDO COM DICIONÁRIOS"""
-precos = map(lambda p: p["Preço"],produtos);
 
-for valor in precos:
-    print(valor)
+# Utilizando funções
+def mul (i):
+    return i * i
+
+x=map (mul, (3, 5, 7, 11, 13))
+print (list (x))
